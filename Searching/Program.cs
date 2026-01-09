0b712f3d7ef9860bbd3b555e983a2dd1be5d2698
abhay9876 <singhabhayrna987@gmail.com>
2026-01-09 15:58:28 +0530

    [Abhay Singh] Add. Searching Problems Complete

---
+++ Searching/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Searching
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //// Find Negative Number
+            //NegativeNumber.Class1();
+
+            //// Find Specific Word
+            //SpecificWord.Class1();
+
+            //// Find Minimum in a sorted rotated array
+            //RotationPoint.Class1();
+
+            //// Find Peak Element
+            //PeakElement.Class1();
+
+            //// Dorted MAtrix
+            //SortedMatrix.Class1();
+
+            ////Occurence Finder
+            //Occurence.Class1();
+
+            // Challenge Searching
+            ChallengeSearching.Searching();
+
+
+        }
+    }
+}
