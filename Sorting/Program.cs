b9e505c22bca91beb14fb5bc186ea43fd797afd4
abhay9876 <singhabhayrna987@gmail.com>
2026-01-09 16:05:40 +0530

    [Abhay Singh] Add . Sorting problem solved

---
+++ Sorting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Sorting
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //// Bubble Sort
+            //Bubble.Sort();
+
+            //// Insertion Sort
+            //Insertion.Sort();
+
+
+            //// Merge Sort
+            //MergeSorting.Sort();
+
+            //// Quick Sort
+            //QuickSorting.Sort();
+
+            //// Selection Sort
+            //SelectionSorting.Sort();
+
+            //// Counting Sort
+            //CountingSort.Sort();
+
+            // Heap Sort
+            HeapSorting.Sort();
+        }
+    }
+}
