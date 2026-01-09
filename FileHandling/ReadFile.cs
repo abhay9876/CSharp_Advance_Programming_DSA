006a009469890c14b4e5a5b533e3f44ce7e2c347
abhay9876 <singhabhayrna987@gmail.com>
2026-01-09 15:46:23 +0530

    [Abhay Singh] Add. using Stream Read operation

---
+++ FileHandling/ReadFile.cs
@@ -0,0 +1,21 @@
+﻿/*Problem 1: Read a File Line by Line Using StreamReader
+Problem: Write a program that uses StreamReader to read a text file line by line and print
+each line to the console.*/
+
+using System;
+using System.IO;
+
+public class ReadFile
+{
+	public static void Class1()
+	{
+		string path = @"C:\Users\vikra\OneDrive\Desktop\C#_Training\CSharp_ADV_DSA\FileHandling\Test.txt";
+		StreamReader sr = new StreamReader(path);
+		string line;
+		while((line = sr.ReadLine()) != null)
+		{
+			Console.WriteLine(line);
+		}
+
+    }
+}
