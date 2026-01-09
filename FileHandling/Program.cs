2da749b36b3a77689b83f18527ad11c938002160
abhay9876 <singhabhayrna987@gmail.com>
2026-01-09 15:49:21 +0530

    [Abhay Singh] Add. Complete FileHandling

---
+++ FileHandling/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace FileHandling
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //// Reverse String
+            //ReverseString.Class1();
+
+            //// Remove Duplicate
+            //RemoveDuplicate.Class1();
+
+            //// Concatenate
+            //Concatenate.Class1();
+
+            //// Compare performance
+            //Compare.Class1();
+
+            //// Read File
+            //ReadFile.Class1();
+
+            //// Count Occurence
+            //CountOccurence.Class1();
+
+            //// Convert Byte to Char
+            //ConvertByte.Class1();
+
+            // ReadAndWrite
+            ReadAndWrite.Class1();
+        }
+    }
+}
