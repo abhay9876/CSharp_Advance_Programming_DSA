5fff8237e54337d563cbf23e51e38e67746eba23
abhay9876 <singhabhayrna987@gmail.com>
2026-01-09 15:45:37 +0530

    [Abhay Singh] Add. using Stream count the occurence of word

---
+++ FileHandling/CountOccurence.cs
@@ -0,0 +1,29 @@
+﻿/*Problem 2: Count the Occurrence of a Word in a File Using StreamReader
+Problem: Write a program that reads a file and counts how many times a specific word
+appears in the file.*/
+
+using System;
+using System.IO;
+
+public class CountOccurence
+{
+	public static void Class1()
+	{
+        string path = @"C:\Users\vikra\OneDrive\Desktop\C#_Training\CSharp_ADV_DSA\FileHandling\Test.txt";
+        StreamReader sr = new StreamReader(path);
+        string line;
+        int count = 0;
+        while ((line = sr.ReadLine()) != null)
+        {
+            for(int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == 'a')
+                {
+                    count++;
+                }
+            }
+            
+        }
+        Console.WriteLine("The Occurence Of Char 'a' is : " + count);
+    }
+}
