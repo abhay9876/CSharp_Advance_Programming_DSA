edbfb36cedc6b177c80aa9b51698c549d12d75e1
abhay9876 <singhabhayrna987@gmail.com>
2026-01-09 15:43:07 +0530

    [Abhay Singh] Add. Compare string performance

---
+++ FileHandling/Compare.cs
@@ -0,0 +1,36 @@
+﻿/*Compare StringBuilder Performance
+Problem: Write a program that compares the performance of StringBuilder for
+concatenating strings multiple times.   */
+
+
+using System;
+using System.Diagnostics;
+using System.Text;
+
+public class Compare
+{
+	public static void Class1()
+	{
+		Stopwatch sw = new Stopwatch();
+		int n = 100000;
+		
+		sw.Start();
+        string s = "";
+        for (int i = 0; i < n; i++)
+		{
+			s += "a";
+		}
+		sw.Stop();
+		Console.WriteLine($"String : {sw.ElapsedMilliseconds}");
+
+		
+		sw.Restart();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++)
+        {
+			sb.Append("b");
+        }
+        sw.Stop();
+        Console.WriteLine($"StringBuilder : {sw.ElapsedMilliseconds}");
+    }
+}
