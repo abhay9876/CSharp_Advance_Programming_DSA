b261d1b314bd70f866e90d4f83dba18fd5796f43
abhay9876 <singhabhayrna987@gmail.com>
2026-01-11 23:26:33 +0530

    [Abhay Singh] Add. Runtime analysis Problems Solved

---
+++ RuntimeAnalysis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace RuntimeAnalysis
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Linear and Binary
+            LargeDataset.Data();
+
+
+            // RunTime Analysis of Concate string and stringbuilder
+            Concatenation.Concatenate();
+
+
+            RunTime Analysis of reading a file using file stream and streamreader
+            StreamComparison.Reading();
+
+
+            //RunTime Analysis with the help of calculating fabonacci
+            Fibonacci.Calculate();
+        }
+    }
+}
