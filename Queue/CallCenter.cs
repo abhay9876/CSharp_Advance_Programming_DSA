1955e0e9fd1df0d93b5e6722596537c467249a15
abhay9876 <singhabhayrna987@gmail.com>
2026-01-11 23:33:02 +0530

    [Abhay Singh] Add. Call Center Problem Solved

---
+++ Queue/CallCenter.cs
@@ -0,0 +1,59 @@
+﻿/*5.Call Center Management System
+Use Case: Incoming calls are queued for available agents.
+OOP Concepts:
+● Interface: CallRequest
+● Polymorphism: Calls could be SupportCall, SalesCall, etc.
+● Encapsulation: Queue handling hidden inside CallCenterManager.*/
+
+using System;
+
+public interface CallRequest
+{
+    void HandleCall();
+}
+
+public class SupportCall : CallRequest
+{
+    public void HandleCall()
+    {
+        Console.WriteLine("Support call is being handled by support agent.");
+    }
+}
+
+public class SalesCall : CallRequest
+{
+    public void HandleCall()
+    {
+        Console.WriteLine("Sales call is being handled by sales agent.");
+    }
+}
+
+public class FeedbackCall : CallRequest
+{
+    public void HandleCall()
+    {
+        Console.WriteLine("Feedback call is being handled by management team.");
+    }
+}
+
+public class CallCenterManager
+{
+    private Queue<CallRequest> calls = new Queue<CallRequest>();
+
+    public void ReceiveCall(CallRequest call)
+    {
+        calls.Enqueue(call);
+        Console.WriteLine("Incoming call added to queue.");
+    }
+
+    public void ProcessCalls()
+    {
+        while (calls.Count > 0)
+        {
+            CallRequest call = calls.Dequeue();
+            call.HandleCall();
+        }
+    }
+}
+
+
