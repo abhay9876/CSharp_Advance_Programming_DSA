3d2ac4b8d44f1a6c1b742b3b47b76c8605697200
abhay9876 <singhabhayrna987@gmail.com>
2026-01-11 23:38:49 +0530

    [Abhay Singh] Add. HashMap Problems Solved

---
+++ HashMap/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HashMap
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Inventory Management
+            Inventory inventory = new InventoryManager();
+            Product p1 = new Electronics("Laptop", 65000);
+            Product p2 = new Cloths("T-Shirt",600);
+            inventory.Add(101, p1);
+            inventory.Add(102, p2);
+            Product product = inventory.Get(101);
+            product.Details();
+
+            // Role-Based Access
+            AuthSystem auth = new AuthSystem();
+            auth.AssignRole("Abhay", new Admin());
+            auth.AssignRole("Goku", new User());
+            Console.WriteLine(auth.CheckAccess("Abhay", "DELETE")); 
+            Console.WriteLine(auth.CheckAccess("Goku", "Read"));
+
+
+            // Grade Calculate
+            GradeService service = new GradeService();
+            service.AddStudent(101, 88, new CBSE());
+            service.AddStudent(102, 88, new GPA());
+            service.PrintGrade(101);
+            service.PrintGrade(102);
+
+            //URl Shortner
+        }
+    }
+}
