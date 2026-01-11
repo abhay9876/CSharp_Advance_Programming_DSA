a21cf98db9372a7e23aeac4a901e8ec305f5bb91
abhay9876 <singhabhayrna987@gmail.com>
2026-01-11 23:44:36 +0530

    [Abhay Singh] Add. Notification Problem Solved

---
+++ LinkedList/NOtification.cs
@@ -0,0 +1,72 @@
+﻿
+/*Notification Retry Mechanism
+Use Case: Failed notifications (e.g., SMS or email) are retried after failure in FIFO order.
+Why LinkedList? Keeps retry order simple and efficient.
+OOP Concepts:
+● Interface: RetryableTask for various retry operations.
+● Polymorphism: Allows different task types to be retried uniformly.
+● Abstraction: Retry logic is hidden.
+● Encapsulation: Linked list maintains retry queue.*/
+
+using System;
+
+public interface IRetryableTask
+{
+    void Retry();
+}
+
+public class SMSRetry : IRetryableTask
+{
+    public void Retry()
+    {
+        Console.WriteLine("Retrying SMS Notification");
+    }
+}
+
+public class EmailRetry : IRetryableTask
+{
+    public void Retry()
+    {
+        Console.WriteLine("Retrying Email Notifications");
+    }
+}
+
+public class RetryBox
+{
+    public IRetryableTask Data;
+    public RetryBox Next;
+
+    public RetryBox(IRetryableTask data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
+
+public class RetryQueue
+{
+    private RetryBox front, rear;
+
+    public void Add(IRetryableTask task)
+    {
+        RetryBox box = new RetryBox(task);
+
+        if (rear == null)
+        {
+            front = rear = box;
+            return;
+        }
+
+        rear.Next = box;
+        rear = box;
+    }
+
+    public void RetryAll()
+    {
+        while (front != null)
+        {
+            front.Data.Retry();  
+            front = front.Next;
+        }
+    }
+}
